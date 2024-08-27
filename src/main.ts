ffae9540990ed4043bfdb2a3973da7654e827813
starkx1900 <Alfredstark01@gmail.com>
2024-08-27 23:02:31 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.setGlobalPrefix('api', { exclude: ['/', 'api-docs'] });
   app.useGlobalPipes(
     new ValidationPipe({
